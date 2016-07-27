8167a04856ff3b1ad9e5a6707f97873f720d99b4
marin.dou <marin.dou@uxin.com>
2016-07-27 16:04:07 +0800

    update get token method

--- Resources/uxin_live.py
+++ Resources/uxin_live.py
@@ -12,8 +12,10 @@ class uxin_live(object):
         cmd_code = "curl -s 'http://%s:%s/v1/user/sendValidateCode' -d 'mobile=%s'" % (serverhost, serverport, mobile)
         res_code = commands.getoutput(cmd_code)
         r = redis.Redis(host=redishost, port=redisport, db = 0, password = redispwd)
-        value = r.get(mobile)
-        code = json.loads(value)
+	key = "sms_item_businesstype_mobile__0_" + str(mobile)
+        value = r.get(key)
+	cmd_code = "echo '%s'|awk -F 'contents\":' '{print $2}'|cut -b 3-6" % (value)
+	code = commands.getoutput(cmd_code)
         cmd_token = "curl -s -i -H '_c:1' 'http://%s:%s/v1/user/cellPhoneLogin' -d 'mobile=%s&code=%s'|grep 'x-auth-token'" % (serverhost, serverport, mobile, code)
         res_token = commands.getoutput(cmd_token)
         token_split = res_token[:-1].split(": ") # 不知道为啥有个换行符
@@ -24,5 +26,5 @@ class uxin_live(object):
 
 if __name__ == '__main__':
     token = uxin_live()
-    print token.get_token(18688410526)
+    print token.get_token(18612069260)
     pass
