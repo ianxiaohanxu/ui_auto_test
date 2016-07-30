746fbc9e373143930947d065fc50a2700a14247b
marin.dou <marin.dou@uxin.com>
2016-07-30 19:11:24 +0800

    add room cases

--- Resources/uxin_live.py
+++ Resources/uxin_live.py
@@ -4,6 +4,7 @@ import commands
 import json
 import redis
 import sys  
+import time
 
 reload(sys)  
 sys.setdefaultencoding('utf8')
@@ -50,17 +51,35 @@ class uxin_live(object):
         code = commands.getoutput(cmd_code)
         return code
 
-    def upload_file(self, token, params= None, files = None, serverhost = '60.205.59.6', serverport = 8080, url = 'user/setUserInfo'):
+    def upload_file(self, token, params= None, files = None, serverhost = '60.205.59.6', serverport = 8080, url = 'user/setUserInfo', mode = 1):
 	token = token.replace('=',':')
-	cmd = "curl -s -F'%s' -H'_c:1' -H'%s' 'http://%s:%s/v1/%s?%s'" % (files, token, serverhost, serverport, url, params)
+	if mode == 1:
+	    cmd = "curl -s -F'%s' -H'_c:1' -H'%s' 'http://%s:%s/v1/%s?%s'" % (files, token, serverhost, serverport, url, params)
+	else :
+	    cmd = "curl -s -H'_c:1' -H'%s' 'http://%s:%s/v1/%s' -d ''" % (token, serverhost, serverport, url)
 	res = commands.getoutput(cmd)
 	return res
 
-	
+    def get_datetime(self, time_dur=0):
+	#返回13位时间戳， time_dur 单位：小时
+        now = time.time()
+	res_time = now*1000 + int(time_dur)*60*60*1000
+	return '%d'%res_time
+
+    def get_url_split(self, str_url, c = '/', pos = 3):
+	arr1 = str_url.split(c)
+	str_tmp = arr1[pos]
+	if '?' in str_tmp:
+	    arr2 = str_tmp.split('?')
+	    return arr2[0]
+	else :
+	    return str_tmp
 
 if __name__ == '__main__':
     user = uxin_live()
     #print user.get_token(18612069260)
     #print user.get_uid(18612069260)
-    print user.upload_file('x-auth-token:559d0efc-d91d-48ae-aead-bcd9f61a72fd', params='introduction=test1352245782&nickname=test1352245782&gender=1', files = 'multipartFile=@/Users/douqianxin/work/robot/auto_test/scripts/interface_test/uxinlive/user/1.png')
+    #print user.upload_file('x-auth-token:559d0efc-d91d-48ae-aead-bcd9f61a72fd', params='introduction=test1352245782&nickname=test1352245782&gender=1', files = 'multipartFile=@/Users/douqianxin/work/robot/auto_test/scripts/interface_test/uxinlive/user/1.png')
+    #print user.get_datetime(-1)
+    #print user.get_url_split('http://uxin-zb-picture.img-cn-shenzhen.aliyuncs.com/p201607306751932937015699373.png?124fsadf')
     pass
