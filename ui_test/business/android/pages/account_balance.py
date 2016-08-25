1a03f86af210754211123247d700b2346d46072a
Vesper_sd <Vesper_sd@sina.com>
2016-08-25 14:58:45 +0800

    新增账户余额脚本

---
+++ ui_test/business/android/pages/account_balance.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8 -*-
+
+## Location info
+## pagename_location_name = "xxxxx"
+# Account Balance
+me_account_balance_back = "com.yx:id/tv_back"
+me_account_balance_title = u"账户余额"
+me_account_balance_cost_record = "com.yx:id/imageview_titlebar_right_view_record"
+me_account_balance_u_icon = "com.yx:id/iv_ubalance"
+me_account_balance_recmduxin = "com.yx:id/rl_item_select"
+
+# Account Balance > recommend uxin
+me_account_balance_recmduxin_back = "com.yx:id/tv_back"
+me_account_balance_recmduxin_title = u"赚U币"
+me_account_balance_recmduxin_qyq = "com.yx:id/share_pyq_layout"
+me_account_balance_recmduxin_qzone = "com.yx:id/share_qzone_layout"
+me_account_balance_recmduxin_wb = "com.yx:id/share_wb_layout"
+me_account_balance_recmduxin_invitefriend = "com.yx:id/invitelayout_download_promptlayout"
