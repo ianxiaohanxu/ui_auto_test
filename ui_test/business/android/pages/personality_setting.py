71e35e3e2742ecd75b6b7000a2db452c7979831e
Vesper_sd <Vesper_sd@sina.com>
2016-08-25 14:59:23 +0800

    新增个性化设置脚本

---
+++ ui_test/business/android/pages/personality_setting.py
@@ -0,0 +1,45 @@
+#-*- coding: utf-8 -*-
+
+## Location info
+## pagename_location_name = "xxxxx"
+personality_setting_back = "com.yx:id/tv_back"
+personality_setting_title = u"个性化设置"
+personality_setting_my = "//android.widget.LinearLayout[@resource-id='com.yx:id/right_view_group']/android.widget.ImageView"
+
+personality_setting_calloutshow = u"去电秀"
+personality_setting_calloutshow_back = "com.yx:id/tv_back"
+personality_setting_calloutshow_title = u"我的去电秀"
+
+personality_setting_theme = u"主题"
+personality_setting_theme_back = "com.yx:id/tv_back"
+personality_setting_theme_title = u"主题"
+
+personality_setting_pressmusic = u"按键音"
+personality_setting_pressmusic_back = "com.yx:id/tv_back"
+personality_setting_pressmusic_title = u"按键音设置"
+
+personality_setting_starclock = u"明星闹钟"
+personality_setting_starclock_back = "com.yx:id/tv_back"
+personality_setting_starclock_title = u"明星闹钟"
+
+#My personality
+personality_setting_my_back = "com.yx:id/tv_back"
+personality_setting_my_title = u"我的个性化"
+
+personality_setting_my_theme = "com.yx:id/myTheme"
+personality_setting_my_theme_back = "com.yx:id/tv_back"
+personality_setting_my_theme_title = u"主题"
+
+personality_setting_my_calloutshow = "com.yx:id/callOutShow"
+personality_setting_my_calloutshow_back = "com.yx:id/tv_back"
+personality_setting_my_calloutshow_title = u"我的去电秀"
+
+personality_setting_my_pressmusic = "com.yx:id/myPressMusic"
+personality_setting_my_pressmusic_back = "com.yx:id/tv_back"
+personality_setting_my_pressmusic_title = u"按键音设置"
+
+personality_setting_my_starclock = "com.yx:id/starClock"
+personality_setting_my_starclock_back = "com.yx:id/tv_back"
+personality_setting_my_starclock_title = u"明星闹钟"
+
+
