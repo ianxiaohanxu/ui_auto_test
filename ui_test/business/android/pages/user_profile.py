37602ceeb313c12ce33a17659c7234c760e2622a
Vesper_sd <Vesper_sd@sina.com>
2016-08-25 15:00:24 +0800

    新增个人资料脚本

---
+++ ui_test/business/android/pages/user_profile.py
@@ -0,0 +1,19 @@
+#-*- coding: utf-8 -*-
+
+## Location info
+## pagename_location_name = "xxxxx"
+# User profile
+me_user_profile_back = "com.yx:id/tv_back"
+me_user_profile_title = u"个人资料"
+me_user_profile_edit = "com.yx:id/iv_random_user_profile_edit"
+me_user_profile_u_icon= "com.yx:id/iv_random_user_uxin"
+
+# Edit user profile page
+me_user_profile_edit_back = "com.yx:id/tv_back"
+me_user_profile_edit_title = u"编辑个人资料"
+me_user_profile_edit_ok = u"确定"
+me_user_profile_edit_msg = u"基本信息"
+me_user_profile_edit_image = u"头像"
+me_user_profile_edit_nickname = u"昵称"
+me_user_profile_edit_phone = u"手机"
+
