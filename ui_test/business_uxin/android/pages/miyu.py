ad6844aed8a8b4ff2ab0e844e721268f781d1b98
alex.gao <alex.gao@uxin.com>
2016-10-27 18:29:50 +0800

    Alex resolves the second conflict

---
+++ ui_test/business_uxin/android/pages/miyu.py
@@ -0,0 +1,30 @@
+#-*- coding: utf-8 -*-
+
+## Location info
+## pagename_location_name = "xxxxx"
+me_miyu_location_back = "com.yx:id/tv_back"
+me_miyu_location_profile = "com.yx:id/iv_my_profile"
+me_miyu_location_setting = "com.yx:id/iv_random_setting"
+me_miyu_location_charming = "com.yx:id/rl_charm"
+me_miyu_location_young = "com.yx:id/rl_young"
+me_miyu_charming_location_back = "com.yx:id/tv_back"
+me_miyu_young_location_back = "com.yx:id/tv_back"
+me_miyu_profile_location_back = "com.yx:id/tv_back"
+me_miyu_setting_location_back = "com.yx:id/tv_back"
+me_miyu_mht_popup_window_location_female = "com.yx:id/tv_select_female"
+me_miyu_mht_popup_window_location_male = "com.yx:id/tv_select_male"
+me_miyu_mht_upload_info_location_back = "com.yx:id/tv_back"
+me_miyu_profile_popup_window_location_fill_info = "com.yx:id/btn_show_profile_complete"
+
+## Verification info
+## pagename_verification_name = "xxxxx"
+me_miyu_verification_title = u"在线"
+me_miyu_charming_verification_title = u"魅力榜"
+me_miyu_young_verification_title = u"新秀榜"
+me_miyu_profile_verification_title = u"个人资料"
+me_miyu_setting_verification_title = u"设置"
+me_miyu_mht_popup_window_verification_title = u"棉花糖，用声音温暖你的心"
+me_miyu_mht_upload_info_verification_image = u"第二步    上传头像"
+me_miyu_profile_popup_window_verification_title = u"完善资料，让更多人认识你"
+me_miyu_more_popup_window_verification_title = u"查看更多棉花糖女生"
+
--- ui_test/business_uxin/ios/pages/all_pages.py
+++ ui_test/business_uxin/ios/pages/all_pages.py
@@ -1,5 +1,3 @@
 #-*- coding: UTF-8 -*-
 from login import *
 from bottom import *
-from me import *
-from settings import *
